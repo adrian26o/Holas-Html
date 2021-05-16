a954425e4d6c477ae8446a2819e413f485760cfc
Zhen <zhen@pop-os.localdomain>
2021-05-15 23:09:06 -0500

    Docs agregados

--- directions.js
+++ directions.js
@@ -4,30 +4,26 @@ const path = require("path")
 
 router.get("/",function(req,resp)
 {
-    resp.render("index.ejs")
+    resp.render("index.ejs");
     resp.end();
 })
 
 router.get("/docs", (req,resp) => 
 {  
-    resp.end("docs")
+    resp.render("docs.ejs");
+    resp.end();
 })
 
 
 router.get("/creditos", (req,resp) => 
 {  
-    resp.render("creditos.ejs")
-})
-
-router.get("/test", (req,resp) =>
-{
-    resp.render("./partials/pie.ejs")
-    resp.end()
+    resp.render("creditos.ejs");
+    resp.end();
 })
 
 router.use((req,res,next)=>
 {
-    res.status(404).render("404.ejs")
+    res.status(404).render("404.ejs");
 })
 
 module.exports = router;
