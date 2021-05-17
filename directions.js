eae6188aab1e93dce0af5aaa7353652ee2255f95
Zhen <zhen@pop-os.localdomain>
2021-05-17 12:55:37 -0500

    Finalizado 12:55pm

--- directions.js
+++ directions.js
@@ -4,26 +4,26 @@ const path = require("path")
 
 router.get("/",function(req,resp)
 {
-    resp.render("main.ejs",{title:'Página principal',description:"¡Hola!"});
+    resp.render("main.ejs",{title:'Dashboard',description:"¡Hola!"});
     resp.end();
 })
 
 router.get("/docs", (req,resp) => 
 {  
-    resp.render("docs.ejs");
+    resp.render("docs.ejs",{title:'Comandos',description:"Lista de comandos disponibles"});
     resp.end();
 })
 
 
 router.get("/creditos", (req,resp) => 
 {  
-    resp.render("creditos.ejs");
+    resp.render("creditos.ejs",{title:'Creditos',description:"Amén"});
     resp.end();
 })
 
 router.use((req,res,next)=>
 {
-    res.status(404).render("404.ejs");
+    res.status(404).render("404.ejs",{title:'404',description:"Página no encontrada"});
 })
 
 module.exports = router;
